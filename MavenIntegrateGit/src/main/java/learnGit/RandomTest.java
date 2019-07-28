eddd89745d53e14743b3cd42b52b520724c0d050
admodi <admodi@ADMODI-LAP.oradev.oraclecorp.com>
2019-07-28 10:38:53 +0530

    This is the file change

--- MavenIntegrateGit/src/main/java/learnGit/RandomTest.java
+++ MavenIntegrateGit/src/main/java/learnGit/RandomTest.java
@@ -5,7 +5,7 @@ public class RandomTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is sample 1");
+		System.out.println("This is sample 1 and updating the text change");
 
 	}
 
