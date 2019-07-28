1489e087045f77eef67dacbb37516138cbe9a7a6
admodi <admodi@ADMODI-LAP.oradev.oraclecorp.com>
2019-07-28 10:34:57 +0530

    This is the first commit

---
+++ MavenIntegrateGit/src/main/java/learnGit/RandomTest.java
@@ -0,0 +1,12 @@
+package learnGit;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is sample 1");
+
+	}
+
+}
